39e5014425e2998e1722c2e53e36650289433c86
Almina Pajazitovska <127589975+sesamino@users.noreply.github.com>
2023-09-29 18:34:40 +0200

    Add files via upload

---
+++ pajazitovska.almina.4i.rubricaWPF/Contatto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pajazitovska.almina._4i.rubrica
+{
+    public class Contatto
+    {
+        private int numero;
+        private string nome;//creo tre classi private, private può essere anche omesso dato che c'è di default
+        private string cognome;
+
+        public int Numero { get => numero; set => numero = value; }//=> 
+        public string Nome { get => nome; set => nome = value; }
+        public string Cognome { get => cognome; set => cognome = value; }
+    }
+}
---
+++ pajazitovska.almina.4i.rubricaWPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pajazitovska.almina._4i.rubrica
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Contatto c = new Contatto();
+            c.Numero = 1;
+            c.Nome = "Nome";
+            c.Cognome = "Cognome";
+            
+            Contatto[] Contatti = new Contatto[100];
+            Contatti [0] = c;
+            
+            Contatti[0].Nome ="Io" ;
+            Contatti[0].Cognome = "Tu";
+
+        }
+    }
+}
